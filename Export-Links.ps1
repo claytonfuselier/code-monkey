5c58dc8dd8616f0b143730bb5bbf2e920cd96c98
Clayton (Msft Azure) <53104388+claytonfuselier@users.noreply.github.com>
2023-05-20 14:57:09 -0500

    Update Export-Links.ps1

--- Export-Links.ps1
+++ Export-Links.ps1
@@ -1,11 +1,11 @@
 ###############
 ##  Summary  ##
 ###############
-# Intended use is on CodeWiki pages in a locally cloned Azure DevOps (or similar local repositiory).
+# Intended use is on CodeWiki pages in a locally cloned Azure DevOps (or similar local repository).
 # Focus is on parsing .md files for URLs with a certain domain name and recording every instance in a CSV. Using a 
 # wildcard to find "all" URLs is not (currently) supported; a domain must be provided.
 #
-# Note: Script accounts for plaintext and html-encoded URLs, as well as http and https.
+# Note: The script accounts for plaintext and HTML-encoded URLs, as well as HTTP and HTTPS.
 #
 # Source: https://github.com/claytonfuselier/KM-Scripts/blob/main/Export-Links.ps1
 
@@ -14,9 +14,9 @@
 ##########################
 ##  Required variables  ##
 ##########################
-$gitroot = "" # Local cloned repository (ex. "C:\Users\<username>\git\<repo>")
-$exportpath = "" # Where to export the CSV (ex. "C:\Users\<username>\Documents")
-$domain = "" # Domain/FQDN to search for (ex. "sub.example.com")
+$gitroot = ""      # Local cloned repository (e.g., "C:\Users\<username>\git\<repo>")
+$exportpath = ""   # Where to export the CSV (e.g., "C:\Users\<username>\Documents")
+$domain = ""       # Domain/FQDN to search for (e.g., "sub.example.com")
 
 
 
@@ -27,12 +27,12 @@ $scriptstarttime = Get-Date
 $pagecnt = 0
 
 # Get pages
-$pages = Get-ChildItem -Path $gitroot -Recurse | where {$_.Extension -eq ".md"}
+$pages = Get-ChildItem -Path $gitroot -Recurse | where { $_.Extension -eq ".md" }
 
 # Parse pages
 $pages | ForEach-Object {
     $pagename = $_.Name
-    $pagepath = ($_.Directory.FullName).Replace($gitroot,"")
+    $pagepath = ($_.Directory.FullName).Replace($gitroot, "")
     Write-Host "$pagepath\$pagename"
 
     # Get page content
@@ -40,19 +40,19 @@ $pages | ForEach-Object {
 
     # Parse each line
     $line = 1
-    $pagecontent | ForEach-Object{
+    $pagecontent | ForEach-Object {
         # Check if line contains a match
-        if($_ -match "$domain"){
-            # Get all matches in the line (accounts for potentially multiples per line)
-            $urls = [regex]::Matches($_, 'https?(:\/\/|%3A%2F%2F)'+$domain+'(\/|%2F)[^)\]}\s>]*', [Text.RegularExpressions.RegexOptions]::IgnoreCase).Value
-            $urls | ForEach-Object{
+        if ($_ -match $domain) {
+            # Get all matches in the line (accounts for potentially multiple matches per line)
+            $urls = [regex]::Matches($_, 'https?(:\/\/|%3A%2F%2F)' + $domain + '(\/|%2F)[^)\]}\s>]*', [System.Text.RegularExpressions.RegexOptions]::IgnoreCase).Value
+            $urls | ForEach-Object {
 
                 # Export results
                 $resultsrow = New-Object psobject
                 $resultsrow | Add-Member -Type NoteProperty -Name "Path" -Value $pagepath
                 $resultsrow | Add-Member -Type NoteProperty -Name "PageName" -Value $pagename
                 $resultsrow | Add-Member -Type NoteProperty -Name "Line Number" -Value $line
-                $resultsrow | Add-Member -Type NoteProperty -Name "URL" -Value "$_"
+                $resultsrow | Add-Member -Type NoteProperty -Name "URL" -Value $_
                 $resultsrow | Export-Csv -Path "$exportpath\Links.csv" -NoTypeInformation -Append
                 Write-Host -ForegroundColor Yellow "Found Link"
             }
@@ -62,9 +62,9 @@ $pages | ForEach-Object {
 
     # Progress bar
     $pagecnt++
-    $avg = ((Get-Date) – $scriptstarttime).TotalMilliseconds/$pagecnt
-    $msleft = (($pages.Count–$pagecnt)*$avg)
-    $time = New-TimeSpan –Seconds ($msleft/1000)
-    $percent = [MATH]::Round(($pagecnt/$pages.Count)*100,2)
+    $avg = ((Get-Date) - $scriptstarttime).TotalMilliseconds / $pagecnt
+    $msleft = (($pages.Count - $pagecnt) * $avg)
+    $time = New-TimeSpan -Seconds ($msleft / 1000)
+    $percent = [Math]::Round(($pagecnt / $pages.Count) * 100, 2)
     Write-Progress -Activity "Searching... ($percent %)" -Status "$pagecnt of $($pages.Count) total pages - $time" -PercentComplete $percent
 }
