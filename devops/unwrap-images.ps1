51e3f1962abd8a240b39a95123351e537f2366d3
Clayton (Msft Azure) <53104388+claytonfuselier@users.noreply.github.com>
2023-11-19 04:13:30 -0600

    Update unwrap-images.ps1

--- devops/unwrap-images.ps1
+++ devops/unwrap-images.ps1
@@ -6,9 +6,9 @@
 # Focus is on parsing .md files for HTML image tags that are wrapped inside HTML anchor (link) tags
 # (e.g., "<a...><img...></a>") and taking one of three actions below.
 #
-# Actions: 1 - Unwrap and convert image to markdown. Ex: "![filename](/path/to/image)"
+# Actions: 1 - Unwrap and convert image to markdown. Ex: "![AltText](/path/to/image)"
 #          2 - Unwrap but leave as HTML.             Ex: "<img...>"
-#          3 - Convert wrap and image to markdown.   Ex: "[![filename](/path/to/image)](URL-to-image)"
+#          3 - Leave wrap and convert to markdown.   Ex: "[![AltText](/path/to/image)](URL-to-image)"
 #
 # Source: https://github.com/claytonfuselier/code-monkey/blob/main/devops/unwrap-images.ps1
 # Help: https://github.com/claytonfuselier/code-monkey/wiki
@@ -23,9 +23,8 @@ $action = 1        # Action Options:
                       # 1: Unwrap and convert image to markdown
                       # 2: Unwrap but leave image as HTML
                       # 3: Convert wrap and image to markdown
-$bonus = 0
 
-######################################################################################## To do: need processing for width, height, and alt (if present). if present, use alt in place of filename.
+
 
 ####################
 ##  Begin Script  ##
@@ -42,50 +41,111 @@ $pageCnt = 0
 $editedPages = 0
 $totalEdits = 0
 $pages | ForEach-Object {
-    $updated = $false
+    # Output current page
     $_.FullName.Replace("$gitRoot","") | Write-Host
 
     # Get contents of page
     $pageContent = Get-Content -Encoding UTF8 -LiteralPath $_.FullName
 
-
     # Check for HTML links wrapped around HTML image tags
     if($pageContent -match "<a[^>]*><img[^>]*><\/a>"){
         # Get all wrapped images
         $wrappedImages = [regex]::Matches($pageContent, "<a[^>]*><img[^>]*><\/a>", [Text.RegularExpressions.RegexOptions]::IgnoreCase)
-        #$plainImages = [regex]::Matches($pageContent, "<img[^>]*>", [Text.RegularExpressions.RegexOptions]::IgnoreCase)
-        
+#        $plainImages = [regex]::Matches($pageContent, "<img[^>]*>", [Text.RegularExpressions.RegexOptions]::IgnoreCase)
+
         # Parse each instance
         $wrappedImages | ForEach-Object{
-            # Get img path
-            $img = [regex]::Matches($_, '<img[^>]*>', [Text.RegularExpressions.RegexOptions]::IgnoreCase)
-            $imgSrc = [regex]::Matches($img, 'src=[^\s]+', [Text.RegularExpressions.RegexOptions]::IgnoreCase)
-            $imgSrcUrl = $imgSrc.Value.Replace("src=","").Replace("`"","")
-            $imgPath = ([regex]::Matches($imgSrc, 'path=[^\s&"]*', [Text.RegularExpressions.RegexOptions]::IgnoreCase)).Value
+            # Get pre-spacing
+            $curWrappedImage = $_
+            $preSpace = ""
+            $pageContent | ForEach-Object{
+                if($_ -match [regex]::Escape($curWrappedImage)){
+                    $preSpace = ([regex]::Matches($_, "^(\t| )*(?=.*" + [regex]::Escape($curWrappedImage) + ")", [Text.RegularExpressions.RegexOptions]::IgnoreCase)).Value
+                }
+            }
 
-            # Fix path
-            $fixedPath = $imgPath.Replace("path=","").Replace("%2f","/").Replace("%2F","/")
+            # Isolate anchor tag and get url
+            $link = [regex]::Matches($_, "<a[^>]*>", [Text.RegularExpressions.RegexOptions]::IgnoreCase)
+            $linkHref = [regex]::Matches($link, "href=(`"|')?[^`"'\s]*(`"|')?", [Text.RegularExpressions.RegexOptions]::IgnoreCase)
+            $linkUrl = $linkHref.Value.Replace("href=","").Replace("`"","")
 
-            # Get filename
-            $fileName = ([regex]::Matches($fixedPath, '[^\/]+\.\w{3,4}$', [Text.RegularExpressions.RegexOptions]::IgnoreCase)).Value
+            # Isolate img tag
+            $img = ([regex]::Matches($_, "<img[^>]*>", [Text.RegularExpressions.RegexOptions]::IgnoreCase)).Value
+
+            # Get and parse img attributes
+            $imgAttribs = ([regex]::Matches($img, "(?<=\s)\w*=[^`"']*`"[^`"']*`"", [Text.RegularExpressions.RegexOptions]::IgnoreCase)).Value
+            $imgAttribs | ForEach-Object{
+                $curAttrib = $_
+                $altText = $null
+                $widthSize = $null
+                $heightSize = $null
+
+                # Split attribute
+                $attribName = $_.Split("=")[0]
+                $attribValue = $_.Split("=")[1]
+                
+                # Process attribute
+                switch ($attribName) {
+                    "src" {
+                        $imgSrcUrl = $curAttrib.Replace("src=","").Replace("`"","")
+                        $imgPath = ([regex]::Matches($imgSrcUrl, "path=[^\s&`"]*", [Text.RegularExpressions.RegexOptions]::IgnoreCase)).Value
+                        $fixedPath = $imgPath.Replace("path=","").Replace("%2f","/").Replace("%2F","/")
+                    }
+                    "alt" {
+                        $altText = $attribValue.Replace("`"","").Replace("'","")
+                    }
+                    "width" {
+                        $widthSize = ([regex]::Matches($attribValue, "\d+", [Text.RegularExpressions.RegexOptions]::IgnoreCase)).Value
+                    }
+                    "height" {
+                        $heightSize = ([regex]::Matches($attribValue, "\d+", [Text.RegularExpressions.RegexOptions]::IgnoreCase)).Value
+                    }
+                }
+            }
+
+            # Format html attributes
+            $htmlAttrib = "src=`"$imgSrcUrl`""
+            if($altText){
+                $htmlAttrib += " alt=`"$altText`""
+            }
+            if($widthSize){
+                $htmlAttrib += " width=`"$widthSize`""
+            }
+            if($heightSize){
+                $htmlAttrib += " height=`"$heightSize`""
+            }
+
+            # Format markdown attributes
+            if($widthSize -and $heightSize){
+                $mdImgSrc = "$fixedPath =$widthSize"+"x"+"$heightSize"
+            }
+            if($widthSize -and -not$heightSize){
+                $mdImgSrc = "$fixedPath =$widthSize"+"x"
+            }
+            if(-not$widthSize -and $heightSize){
+                $mdImgSrc = "$fixedPath =x$heightSize"
+            }
 
             # Take $action and create replacement line
             switch ($action) {
-                {$_ -le 1} {$newLine = "![$fileName]($fixedPath)"}
-                2 {$newLine = "<img $imgSrc>"}
-                3 {$newLine = "[![$fileName]($fixedPath)]($imgSrcUrl)"}
+                {$_ -le 1} {$newLine = "`n`n$preSpace![$altText]($mdImgSrc)"}
+                2 {$newLine = "<img $htmlAttrib>"}
+                3 {$newLine = "`n`n$preSpace[![$altText]($mdImgSrc)]($imgSrcUrl)"}
             }
-            $pageContent = $pageContent -replace [Regex]::Escape("$_"), $newLine
+
+            # Replace content
+            $pageContent = $pageContent -replace [Regex]::Escape("curWrappedImage"), $newLine
             Write-Host -ForegroundColor Cyan "Updated image..."
             $updated = $true
             $totalEdits++
         }
     }
-    
+
     # Save modified page content
     if($updated){
         Set-Content -Encoding UTF8 -LiteralPath $_.FullName -Value $pageContent
-        Write-Host -ForegroundColor Yellow "Saved $($_.FullName.Replace($gitRoot+"\",''))!"
+        Write-Host -ForegroundColor Yellow "Saved!"
+        $updated = $false
         $editedPages++
     }
 
