1f8be62e7b83b19bfa26395d372d784d2e96ee11
Clayton (Msft Azure) <53104388+claytonfuselier@users.noreply.github.com>
2023-11-18 18:46:41 -0600

    Update unwrap-images.ps1

--- devops/unwrap-images.ps1
+++ devops/unwrap-images.ps1
@@ -2,9 +2,13 @@
 ##  Summary  ##
 ###############
 # Intended use is on CodeWiki pages in a locally cloned Azure DevOps (or similar local repository).
-# Focus is on parsing .md files for HTML image tags that are wrapped in HTML anchor tags
-# (e.g., "<a...><img...></a>") and unwrapping them, while simultaneously converting them to 
-# markdown syntax (e.g., "![filename](/path/to/image)")
+#
+# Focus is on parsing .md files for HTML image tags that are wrapped inside HTML anchor (link) tags
+# (e.g., "<a...><img...></a>") and taking one of three actions below.
+#
+# Actions: 1 - Unwrap and convert image to markdown. Ex: "![filename](/path/to/image)"
+#          2 - Unwrap but leave as HTML.             Ex: "<img...>"
+#          3 - Convert wrap and image to markdown.   Ex: "[![filename](/path/to/image)](URL-to-image)"
 #
 # Source: https://github.com/claytonfuselier/code-monkey/blob/main/devops/unwrap-images.ps1
 # Help: https://github.com/claytonfuselier/code-monkey/wiki
@@ -15,6 +19,10 @@
 ##  Required variables  ##
 ##########################
 $gitRoot = ""      # Local cloned repository (e.g., "<drive>:\path\to\repo")
+$action = 1        # Action Options:
+                      # 1: Unwrap and convert image to markdown
+                      # 2: Unwrap but leave image as HTML
+                      # 3: Convert wrap and image to markdown
 
 
 
@@ -25,65 +33,68 @@ $scriptStartTime = Get-Date
 
 
 # Get all pages
-$pages = Get-ChildItem -Recurse $gitroot -Include *.md | where {! $_.PSIsContainer}
-$pagecnt = 0
-$updpagecnt = 0
-$updimgcnt = 0
+$pages = Get-ChildItem -Recurse $gitRoot -Include *.md | where {! $_.PSIsContainer}
 
 
 # Parse each page
+$pageCnt = 0
+$editedPages = 0
+$totalEdits = 0
 $pages | ForEach-Object {
-    $updated = "false"
-    $_.FullName.Replace("$gitroot","") | Write-Host
+    $updated = $false
+    $_.FullName.Replace("$gitRoot","") | Write-Host
 
     # Get contents of page
-    $pagecontent = Get-Content -Encoding UTF8 -LiteralPath $_.FullName
+    $pageContent = Get-Content -Encoding UTF8 -LiteralPath $_.FullName
+
 
     # Check for HTML links wrapped around HTML image tags
-    if($pagecontent -match "<a\s.+<img.+\/a>"){
+    if($pageContent -match "<a\s.+<img.+\/a>"){
         # Get all wrapped images
-#        $wrappedimages = [regex]::Matches($pagecontent, "(<br\/>)*<a[^>]*><img[^>]*><\/a>(<br\/?>)*", [Text.RegularExpressions.RegexOptions]::IgnoreCase)
-        $wrappedimages = [regex]::Matches($pagecontent, "<a[^>]*><img[^>]*><\/a>", [Text.RegularExpressions.RegexOptions]::IgnoreCase)
+        $wrappedImages = [regex]::Matches($pageContent, "<a[^>]*><img[^>]*><\/a>", [Text.RegularExpressions.RegexOptions]::IgnoreCase)
         
         # Parse each instance
-        $wrappedimages | ForEach-Object{
+        $wrappedImages | ForEach-Object{
+            # Get img path
             $img = [regex]::Matches($_, '<img[^>]*>', [Text.RegularExpressions.RegexOptions]::IgnoreCase)
-            $imgsrc = [regex]::Matches($img, 'src=[^\s]+', [Text.RegularExpressions.RegexOptions]::IgnoreCase)
-            $path = ([regex]::Matches($imgsrc, 'path=[^\s&"]*', [Text.RegularExpressions.RegexOptions]::IgnoreCase)).Value
-#            Write-Host -ForegroundColor Cyan "origpath: $path"
+            $imgSrc = [regex]::Matches($img, 'src=[^\s]+', [Text.RegularExpressions.RegexOptions]::IgnoreCase)
+            $imgSrcUrl = $imgSrc.Value.Replace("src=","").Replace("`"","")
+            $imgPath = ([regex]::Matches($imgSrc, 'path=[^\s&"]*', [Text.RegularExpressions.RegexOptions]::IgnoreCase)).Value
 
-            # Fixing/Cleaning path
-            $fixedpath = $path.Replace("path=","").Replace("%2f","/").Replace("%2F","/")
-#            Write-Host -ForegroundColor Green "fixedpath: $fixedpath"
+            # Fix path
+            $fixedPath = $imgPath.Replace("path=","").Replace("%2f","/").Replace("%2F","/")
 
-            # Convert to markdown syntax (without wrapped link)
-            $filename = ([regex]::Matches($fixedpath, '[^\/]+\.\w{3,4}$', [Text.RegularExpressions.RegexOptions]::IgnoreCase)).Value
-#            $mdimg = "![$filename]($fixedpath)"
-            $replacement = "![$filename]($fixedpath)"
-            $pagecontent = $pagecontent -replace [Regex]::Escape("$_"), $replacement
-            Write-Host -ForegroundColor Cyan "Unwrapped and converted image."
-            $updated = "true"
-            $updimgcnt++
+            # Get filename
+            $fileName = ([regex]::Matches($fixedPath, '[^\/]+\.\w{3,4}$', [Text.RegularExpressions.RegexOptions]::IgnoreCase)).Value
+
+            # Take $action and create replacement line
+            switch ($action) {
+                {$_ -le 1} {$newLine = "![$fileName]($fixedPath)"}
+                2 {$newLine = "<img $imgSrc>"}
+                3 {$newLine = "[![$fileName]($fixedPath)]($imgSrcUrl)"}
+            }
+            $pageContent = $pageContent -replace [Regex]::Escape("$_"), $newLine
+            Write-Host -ForegroundColor Cyan "Updated image..."
+            $updated = $true
+            $totalEdits++
         }
     }
-
-
+    
     # Save modified page content
-    if($updated -eq "true"){
-        Set-Content -Encoding UTF8 -LiteralPath $_.FullName -Value $pagecontent
-        Write-Host -ForegroundColor Yellow "Updated! $($_.FullName)"
-        $updpagecnt++
+    if($updated){
+        Set-Content -Encoding UTF8 -LiteralPath $_.FullName -Value $pageContent
+        Write-Host -ForegroundColor Yellow "Saved $($_.FullName.Replace($gitRoot+"\",''))!"
+        $editedPages++
     }
 
-
     # Progress bar
-    $pagecnt++
-    $avg = ((Get-Date) – $scriptstarttime).TotalMilliseconds/$pagecnt
-    $msleft = (($pages.Count–$pagecnt)*$avg)
-    $time = New-TimeSpan –Seconds ($msleft/1000)
-    $percent = [MATH]::Round(($pagecnt/$pages.Count)*100,2)
-    Write-Progress -Activity "Unwrapping Images ($percent %)" -Status "$pagecnt of $($pages.Count) total pages - $time" -PercentComplete $percent
-
+    $pageCnt++
+    $avg = ((Get-Date) – $scriptStartTime).TotalMilliseconds/$pageCnt
+    $msLeft = (($pages.Count–$pageCnt)*$avg)
+    $time = New-TimeSpan –Seconds ($msLeft/1000)
+    $percent = [MATH]::Round(($pageCnt/$pages.Count)*100,2)
+    Write-Progress -Activity "Unwrapping Images ($percent %)" -Status "$pageCnt of $($pages.Count) total pages - $time" -PercentComplete $percent
 }
 
-Write-Host "Unwrapped Images: $updimgcnt ($updpagecnt pages)"
+Write-Host -ForegroundColor Yellow "Pages updated: $editedPages"
+Write-Host -ForegroundColor Yellow "Image links updated: $totalEdits"
